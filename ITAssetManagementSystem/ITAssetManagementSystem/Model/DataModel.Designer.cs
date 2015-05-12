e78c6b3d239717b2c22f9d16920bef5c0552250a
shuvoinfinity <shuvo.ndc6011@gmail.com>
2015-05-12 15:36:46 +0600

    Data Model & webconfig changes

--- ITAssetManagementSystem/ITAssetManagementSystem/Model/DataModel.Designer.cs
+++ ITAssetManagementSystem/ITAssetManagementSystem/Model/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Devlopment\Undone Project\ITAssetManagementSystem\ITAssetManagementSystem\Model\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\BDRND2\Source\Repos\AssetManagementSystem\ITAssetManagementSystem\ITAssetManagementSystem\Model\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
