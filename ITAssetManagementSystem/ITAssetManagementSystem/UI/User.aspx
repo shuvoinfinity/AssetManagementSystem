fd6cefd8ea90c6274737c0301fc6815e66aa6a99
shuvoinfinity <shuvo.ndc6011@gmail.com>
2015-05-26 00:56:03 +0600

    Version 0.2

--- ITAssetManagementSystem/ITAssetManagementSystem/UI/User.aspx
+++ ITAssetManagementSystem/ITAssetManagementSystem/UI/User.aspx
@@ -1,4 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="ITAssetManagementSystem.UI.User" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    
+ <div class="row">
+     <asp:Label ID="lblUser" runat="server"></asp:Label>
+     <h2 class="lead"> Users List </h2>
+     <asp:GridView ID="GridUsers" runat="server">
+         
+     </asp:GridView>
+ </div>   
 </asp:Content>
--- ITAssetManagementSystem/ITAssetManagementSystem/UI/User.aspx.cs
+++ ITAssetManagementSystem/ITAssetManagementSystem/UI/User.aspx.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ITAssetManagementSystem.Model;
 
 namespace ITAssetManagementSystem.UI
 {
+  
     public partial class User : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            GetAllUsers();
+        }
 
+        private void GetAllUsers()
+        {
+            try
+            {
+                using (var db = new FixAssetDB())
+                {
+                    var users = db.Users.ToList();
+                    GridUsers.DataSource = users;
+                    GridUsers.DataBind();
+                }
+            }
+            catch (Exception)
+            {
+                lblUser.Text = "User Not Found";
+            }
         }
     }
 }
